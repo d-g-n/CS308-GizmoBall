94378fd7f524c306b79c61077abcecae69b7d5ad
decyg <declangoldneilson@hotmail.com>
2016-01-27 19:50:52 +0000

    Mockups of gui done in intellij, will probably make them by hand later but right now these work for illustration

---
+++ src/view/DesignViewMockup.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Declan on 27/01/2016.
+ */
+public class DesignViewMockup {
+	private JPanel panel1;
+	private JButton designViewButton;
+	private JButton playViewButton;
+	private JTextArea thisIsSupposedToTextArea;
+	private JPanel ButtonPanel;
+	private JPanel GamePanel;
+	private JPanel ControlPanel;
+	private JTextField infoBoxIeFileTextField;
+	private JTextField statusBoxIeItemTextField;
+	private JComboBox comboBox1;
+	private JButton placeElementButton;
+	private JButton moveElementButton;
+	private JButton deleteElementButton;
+}
---
+++ src/view/PlayViewMockup.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Declan on 27/01/2016.
+ */
+public class PlayViewMockup {
+	private JPanel panel1;
+	private JButton designViewButton;
+	private JButton playViewButton;
+	private JTextArea thisIsSupposedToTextArea;
+	private JPanel ButtonPanel;
+	private JPanel GamePanel;
+	private JPanel ControlPanel;
+	private JTextField infoBoxIeFileTextField;
+	private JTextField statusBoxIeRunningTextField;
+	private JButton playButton;
+	private JButton pauseButton;
+	private JButton tickButton;
+}
