9360298ee252809a89a4116c85dbd132c06c2811
btb13184 <btb13184@CIS-KELVIN-40.cis.strath.ac.uk>
2016-02-10 15:25:19 +0000

    Added all the cases of the actionPerformed method

--- src/controller/RunListener.java
+++ src/controller/RunListener.java
@@ -5,19 +5,31 @@ import java.awt.event.ActionListener;
 
 import view.RunGUI;
 
-public class RunListener implements ActionListener{
+public class RunListener implements ActionListener {
 
 	private RunGUI gui;
-	
-	public RunListener(RunGUI gui){
+
+	public RunListener(RunGUI gui) {
 		this.gui = gui;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switch(e.getActionCommand()){
-		case "Exit": System.exit(0); break;
+		switch (e.getActionCommand()) {
+		case "Exit":
+			System.exit(0);
+			break;
+		case "Stop":
+			/* Kill execution; */ break;
+		case "Tick":
+			/* Update only one frame */ break;
+		case "Build Mode":
+			/* Switch to build mode view */ break;
+		case "Settings":
+			/* Change the settings of the project */ break;
+		case "About":
+			/* Some general information about the project */ break;
 		}
 	}
-   
-   }
+
+}
