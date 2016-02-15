c012ae1cd1036dbe0f927f1afdb44f3f5106778c
JCribbes <skb13169@uni.strath.ac.uk>
2016-02-15 22:03:31 +0000

    Added methods to calculate Gravity and Friction. Added ball getter method.

--- src/MainInit.java
+++ src/MainInit.java
@@ -23,27 +23,6 @@ public class MainInit {
         // init model
         ProjectManager pm = new ProjectManager();
 
-        pm.addGizmo(new OuterWall(0,0,totalWidth,0,0));
-        pm.addGizmo(new OuterWall(0,totalWidth,0,totalWidth,0));
-        pm.addGizmo(new OuterWall(0,0,0,totalWidth,0));
-        pm.addGizmo(new OuterWall(totalWidth,0,totalWidth,0,0));
-        
-        pm.addGizmo(new SquareBumper(10, 10, 1, 1, 0));
-        pm.addGizmo(new SquareBumper(9, 10, 1, 1, 0));
-        pm.addGizmo(new SquareBumper(11, 10, 1, 1, 0));
-
-        pm.addGizmo(new SquareBumper(12, 9, 1, 1, 0));
-        pm.addGizmo(new SquareBumper(8, 9, 1, 1, 0));
-
-        pm.addGizmo(new SquareBumper(9, 7, 1, 1, 0));
-        pm.addGizmo(new SquareBumper(11, 7, 1, 1, 0));
-
-        pm.addGizmo(new TriangleBumper(1, 1, 1, 1, 0));
-        pm.addGizmo(new TriangleBumper(2, 1, 1, 1, 90));
-
-        pm.addGizmo(new CircularBumper(10, 1, 1, 1, 0));
-
-        pm.addGizmo(new LeftFlipper(15, 15, 0, 0, 0));
 
         pm.addGizmo(new Absorber(1, 18, 18, 1, 0));
 
