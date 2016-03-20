2bdd5471138ead3720349f99c2cee832e83ead44
decyg <declangoldneilson@hotmail.com>
2016-03-20 20:44:22 +0000

    Minor file tidying, keypresslistener was redundant and empty and moved CollisionDetails into CollisionManager as it is essentially like a struct/tuple

--- src/model/CollisionManager.java
+++ src/model/CollisionManager.java
@@ -275,4 +275,31 @@ public class CollisionManager extends Observable {
 	public double getMu2Friction() {
 		return this.SETTINGS_FRICTION_MU2;
 	}
+
+	public class CollisionDetails {
+
+		private Vect velocity, velocity2;
+		private double time;
+		private AbstractGizmo hitGizmo;
+
+		public CollisionDetails(Vect velocity1, Vect velocity2, double time, AbstractGizmo hitGiz){
+			this.velocity = velocity1;
+			this.velocity2 = velocity2;
+			this.time = time;
+			this.hitGizmo = hitGiz;
+		}
+
+		public Vect getVelocity(){
+			return velocity;
+		}
+		public Vect getOtherVelocity(){
+			return velocity2;
+		}
+
+		public double getTimeToCollision(){
+			return time;
+		}
+
+		public AbstractGizmo getHitGizmo() { return hitGizmo; }
+	}
 }
