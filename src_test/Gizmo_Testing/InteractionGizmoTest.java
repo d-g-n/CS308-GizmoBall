b5d46f804673a795a6c85bb028b645e1226c2da5
decyg <declangoldneilson@hotmail.com>
2016-03-20 19:13:04 +0000

    Almost finished test cases, seems to match the spec, dunno if i should do tests for expected collisions and/or change in velocity per tick stuff, might be a pain in the arse.

--- src_test/Gizmo_Testing/InteractionGizmoTest.java
+++ src_test/Gizmo_Testing/InteractionGizmoTest.java
@@ -2,8 +2,12 @@ package Gizmo_Testing;
 
 import General_Testing.BaseTestCase;
 import gizmos.Absorber;
+import gizmos.LeftFlipper;
 import gizmos.SquareBumper;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.AbstractMap;
 
 /**
  * Created by Declan on 20/03/2016.
@@ -17,8 +21,120 @@ public class InteractionGizmoTest extends BaseTestCase {
 
 		Absorber testAbsorber = new Absorber(0, 1, 1, 10);
 
+		LeftFlipper testFlipper = new LeftFlipper(5, 5);
+
 		pm.addGizmo(testSquare);
 
 		pm.addGizmo(testAbsorber);
+
+		pm.addGizmo(testFlipper);
+
+		// test is same as file loading except is for project manager methods
+
+		Assert.assertEquals("Should have 0 listeners", 0, pm.getGizmoByName(testSquare.getName()).getGizmoListeners().size());
+
+		pm.getGizmoByName(testSquare.getName()).addGizmoListener(testAbsorber);
+
+		Assert.assertEquals("Should only have one listener", 1, pm.getGizmoByName(testSquare.getName()).getGizmoListeners().size());
+
+		Assert.assertTrue("First listener should be testAbsorber", pm.getGizmoByName(testSquare.getName()).getGizmoListeners().get(0).getName().equals(testAbsorber.getName()));
+
+		// now test for multiple listeners on one gizmo firing events
+
+		pm.getGizmoByName(testSquare.getName()).addGizmoListener(testFlipper);
+
+		Assert.assertEquals("Should now have two listeners", 2, pm.getGizmoByName(testSquare.getName()).getGizmoListeners().size());
+
+		Assert.assertTrue("Second listener should be testFlipper", pm.getGizmoByName(testSquare.getName()).getGizmoListeners().get(1).getName().equals(testFlipper.getName()));
+
+	}
+
+	@Test
+	public void canGizmoKeyConnects(){
+
+		SquareBumper testSquare = new SquareBumper(0, 0, 1, 1);
+
+		Absorber testAbsorber = new Absorber(0, 1, 1, 10);
+
+		LeftFlipper testFlipper = new LeftFlipper(5, 5);
+
+		pm.addGizmo(testSquare);
+
+		pm.addGizmo(testAbsorber);
+
+		pm.addGizmo(testFlipper);
+
+		// test is same as file loading except is for project manager methods
+
+		Assert.assertNull("Should only have 0 keyconnects for that mapping", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)));
+
+		pm.addKeyConnect(testAbsorber.getName(), 32, "down");
+
+		Assert.assertNotNull("Should now have keyconnects for that mapping", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)));
+
+		Assert.assertTrue("First connection should be to testAbsorber", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)).get(0).equals(pm.getGizmoByName(testAbsorber.getName())));
+
+		// test that we can have multiple gizmos connected to one key
+
+		pm.addKeyConnect(testFlipper.getName(), 32, "down");
+
+		Assert.assertTrue("Second connection should be to testFlipper", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)).get(1).equals(pm.getGizmoByName(testFlipper.getName())));
+
+
+	}
+
+	@Test
+	public void canGizmoDisconnect(){
+
+		// repeated code but it's easier this way rather than iterating to find the name
+
+		SquareBumper testSquare = new SquareBumper(0, 0, 1, 1);
+
+		Absorber testAbsorber = new Absorber(0, 1, 1, 10);
+
+		pm.addGizmo(testSquare);
+
+		pm.addGizmo(testAbsorber);
+
+		pm.getGizmoByName(testSquare.getName()).addGizmoListener(testAbsorber);
+
+		Assert.assertEquals("Should have 1 listener", 1, pm.getGizmoByName(testSquare.getName()).getGizmoListeners().size());
+
+		Assert.assertTrue("First listener should be A", pm.getGizmoByName(testSquare.getName()).getGizmoListeners().get(0).getName().equals(testAbsorber.getName()));
+
+		pm.getGizmoByName(testSquare.getName()).removeGizmoListener(testAbsorber);
+
+		Assert.assertEquals("Should have 0 listeners", 0, pm.getGizmoByName(testSquare.getName()).getGizmoListeners().size());
+
+	}
+
+	@Test
+	public void canGizmoKeyDisconnect(){
+
+		SquareBumper testSquare = new SquareBumper(0, 0, 1, 1);
+
+		Absorber testAbsorber = new Absorber(0, 1, 1, 10);
+
+		LeftFlipper testFlipper = new LeftFlipper(5, 5);
+
+		pm.addGizmo(testSquare);
+
+		pm.addGizmo(testAbsorber);
+
+		pm.addGizmo(testFlipper);
+
+		// test is same as file loading except is for project manager methods
+
+		Assert.assertNull("Should only have 0 keyconnects for that mapping", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)));
+
+		pm.addKeyConnect(testAbsorber.getName(), 32, "down");
+
+		Assert.assertNotNull("Should now have keyconnects for that mapping", pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)));
+
+		pm.removeKeyConnect(testAbsorber.getName(), 32, "down");
+
+		Assert.assertEquals("Should only have 0 keyconnects for that mapping after removal", 0, pm.getKeyConnects().get(new AbstractMap.SimpleEntry<String, Integer>("down", 32)).size());
+
+
 	}
 }
--- src_test/Project_Testing/GizmoFunctionsTest.java
+++ src_test/Project_Testing/GizmoFunctionsTest.java
@@ -1,8 +1,13 @@
 package Project_Testing;
 
 import General_Testing.BaseTestCase;
+import gizmos.Absorber;
+import gizmos.Ball;
+import gizmos.SquareBumper;
+import gizmos.TriangleBumper;
 import junit.framework.Assert;
 import org.junit.Test;
+import physics.Vect;
 
 /**
  * Created by Declan on 12/03/2016.
@@ -11,6 +16,47 @@ public class GizmoFunctionsTest extends BaseTestCase {
 
 	@Test
 	public void testOverlappingGizmos(){
+
+		// test regular overlapping
+
+		SquareBumper testSquare = new SquareBumper(19, 19, 1, 1);
+
+		pm.addGizmo(testSquare);
+
+		TriangleBumper testTriangle = new TriangleBumper(19, 19, 1, 1);
+
+		Assert.assertFalse("Should be false as 19/19 is taken", pm.canPlaceGizmoAt(testTriangle));
+
+		Absorber testAbsorber = new Absorber(0, 0, 5, 5); // should occupy all tiles from 0, 0 to 4, 4
+
+		pm.addGizmo(testAbsorber);
+
+		// should not be able to place anything at 0, 0, 2, 2 or 4, 4
+
+		SquareBumper sq00 = new SquareBumper(0, 0, 1, 1);
+
+		SquareBumper sq22 = new SquareBumper(2, 2, 1, 1);
+
+		SquareBumper sq44 = new SquareBumper(4, 4, 1, 1);
+
+		Assert.assertFalse("Should be false as Absorber takes up 0, 0 -> 4, 4", pm.canPlaceGizmoAt(sq00));
+
+		Assert.assertFalse("Should be false as Absorber takes up 0, 0 -> 4, 4", pm.canPlaceGizmoAt(sq22));
+
+		Assert.assertFalse("Should be false as Absorber takes up 0, 0 -> 4, 4", pm.canPlaceGizmoAt(sq22));
+
+
+		// now test ball placement, note that there's an absorber at 0, 0 and a square at 19, 19
+		// should be able to place ball inside absorber but not inside square
+
+		Ball b00 = new Ball(0, 0, new Vect(0, 0));
+
+		Ball b1919 = new Ball(19, 19, new Vect(19, 19));
+
+		Assert.assertTrue("Should be able to place the ball inside the absorber", pm.canPlaceGizmoAt(b00));
+
+		Assert.assertFalse("Shoiuld not be able to add ball inside the square", pm.canPlaceGizmoAt(b1919));
+
 
 	}
 
